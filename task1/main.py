6b422167144ba6256b5577e1454b43ff43b5b488
wyf~ <cater@mail.ustc.edu.cn>
2023-04-03 19:40:38 +0800

    finish the widget

--- task1/main.py
+++ task1/main.py
@@ -1,14 +1,14 @@
-import utils,os,sys,numpy,json,cv2,lxml,bs4
-from bs4 import BeautifulSoup
-from utils import findweb
-from utils import getart_name
-print("please input the name of researcher")
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from utils import MyApp
 
-name=input()
 
-url=findweb(name)
-art_name=getart_name(url)
-print(art_name)
+if __name__ == "__main__":  #主程序入口
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec())
+
 
 
 
